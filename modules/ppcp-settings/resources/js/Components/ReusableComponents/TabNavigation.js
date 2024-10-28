69ca811b968157cedc89037c056efafcb6f0c864
Philipp Stracker <p.stracker@syde.com>
2024-10-28 15:56:11 +0100

    ✨ Set container width based on active tab panel

--- modules/ppcp-settings/resources/js/Components/ReusableComponents/TabNavigation.js
+++ modules/ppcp-settings/resources/js/Components/ReusableComponents/TabNavigation.js
@@ -1,13 +1,18 @@
 import { TabPanel } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 
 const TabNavigation = ( { tabs } ) => {
+	const initialPanel = tabs[ 0 ].name;
+	const [ activePanel, setActivePanel ] = useState( initialPanel );
 
 	const updatePanelUri = ( tabName ) => {
+		setActivePanel( tabName );
 	};
 
 	return (
 		<TabPanel
-			className={ `ppcp-r-tabs ${ panel }` }
+			className={ `ppcp-r-tabs ${ activePanel }` }
+			initialTabName={ activePanel }
 			onSelect={ updatePanelUri }
 			tabs={ tabs }
 		>
