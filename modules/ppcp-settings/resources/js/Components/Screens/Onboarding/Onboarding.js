05657b8b27d7996c7cc179ef9c54ff0eae42e070
Philipp Stracker <p.stracker@syde.com>
2024-10-31 15:11:36 +0100

    ♻️ Refactor Onboarding component and step handling

--- modules/ppcp-settings/resources/js/Components/Screens/Onboarding/Onboarding.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Onboarding/Onboarding.js
@@ -2,43 +2,34 @@ import Container from '../../ReusableComponents/Container';
 import { useOnboardingStep } from '../../../data';
 import { getSteps } from './availableSteps';
 
-const Onboarding = () => {
-	const { step, setStep, setCompleted, flags } = useOnboardingStep();
-
-	return (
-		<Container page="onboarding">
-			<div className="ppcp-r-card">
-				<OnboardingStep
-					currentStep={ step }
-					setStep={ setStep }
-					setCompleted={ setCompleted }
-					flags={ flags }
-				/>
-			</div>
-		</Container>
-	);
-};
-
-const OnboardingStep = ( { currentStep, setStep, setCompleted, flags } ) => {
-	const stepperOrder = getSteps( flags );
-
+const getCurrentStep = ( requestedStep, steps ) => {
 	const isValidStep = ( step ) =>
 		typeof step === 'number' &&
 		Number.isInteger( step ) &&
 		step >= 0 &&
-		step < stepperOrder.length;
+		step < steps.length;
 
-	const safeCurrentStep = isValidStep( currentStep ) ? currentStep : 0;
+	const safeCurrentStep = isValidStep( requestedStep ) ? requestedStep : 0;
+	return steps[ safeCurrentStep ];
+};
 
-	const CurrentStepComponent = stepperOrder[ safeCurrentStep ];
+const Onboarding = () => {
+	const { step, setStep, setCompleted, flags } = useOnboardingStep();
+	const steps = getSteps( flags );
+
+	const CurrentStepComponent = getCurrentStep( step, steps );
 
 	return (
-		<CurrentStepComponent
-			setStep={ setStep }
-			currentStep={ safeCurrentStep }
-			setCompleted={ setCompleted }
-			stepperOrder={ stepperOrder }
-		/>
+		<Container page="onboarding">
+			<div className="ppcp-r-card">
+				<CurrentStepComponent
+					setStep={ setStep }
+					currentStep={ step }
+					setCompleted={ setCompleted }
+					stepperOrder={ steps }
+				/>
+			</div>
+		</Container>
 	);
 };
 
