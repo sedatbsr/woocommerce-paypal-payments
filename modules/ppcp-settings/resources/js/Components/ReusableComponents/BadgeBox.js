54cbafe1a1622f11cc3833241e6614d4cae6f9bb
Narek Zakarian <narekzakarian@gmail.com>
2024-11-06 18:37:56 +0400

    Adjust the coding styles

--- modules/ppcp-settings/resources/js/Components/ReusableComponents/BadgeBox.js
+++ modules/ppcp-settings/resources/js/Components/ReusableComponents/BadgeBox.js
@@ -1,38 +1,36 @@
 import data from '../../utils/data';
-import TitleBadge, {TITLE_BADGE_INFO} from "./TitleBadge";
-import {__} from "@wordpress/i18n";
+import TitleBadge, { TITLE_BADGE_INFO } from "./TitleBadge";
+import { __ } from "@wordpress/i18n";
 
 const BadgeBox = ( props ) => {
     const titleSize = props.titleType && props.titleType === BADGE_BOX_TITLE_BIG ? BADGE_BOX_TITLE_BIG : BADGE_BOX_TITLE_SMALL
 
-    const titleTextClassName = 'ppcp-r-badge-box__title-text ' + `ppcp-r-badge-box__title-text--${titleSize}`;
+    const titleTextClassName = 'ppcp-r-badge-box__title-text ' + `ppcp-r-badge-box__title-text--${ titleSize }`;
 
     const titleBaseClassName = 'ppcp-r-badge-box__title';
-    const titleClassName = props.imageBadge ? `${titleBaseClassName} ppcp-r-badge-box__title--has-image-badge` : titleBaseClassName;
+    const titleClassName = props.imageBadge ? `${ titleBaseClassName } ppcp-r-badge-box__title--has-image-badge` : titleBaseClassName;
 	return (
         <div className="ppcp-r-badge-box">
-            <span className={titleClassName}>
-                <span className={titleTextClassName}>{props.title}</span>
+            <span className={ titleClassName }>
+                <span className={ titleTextClassName }>{props.title}</span>
 
-                    {props.imageBadge && (
+                    { props.imageBadge && (
                         <span className="ppcp-r-badge-box__title-image-badge">
-                            {props.imageBadge.map((badge) => data().getImage(badge))}
+                            { props.imageBadge.map( ( badge ) => data().getImage( badge ) ) }
                         </span>
-                )}
+                ) }
 
-                {props.textBadge && (
-                    <TitleBadge type={TITLE_BADGE_INFO} text={props.textBadge}/>
-                )}
+                { props.textBadge && (
+                    <TitleBadge type={ TITLE_BADGE_INFO } text={ props.textBadge }/>
+                ) }
             </span>
             <div className="ppcp-r-badge-box__description">
-                {props?.description && (
+                { props?.description && (
                     <p
                         className="ppcp-r-badge-box__description"
-                        dangerouslySetInnerHTML={{
-                            __html: props.description,
-                        }}
+                        dangerouslySetInnerHTML={ { __html: props.description, } }
                     ></p>
-                )}
+                ) }
             </div>
         </div>
     );
