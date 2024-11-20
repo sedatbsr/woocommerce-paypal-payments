329135d1dc799ad24625d7f6dd3249d002988036
Philipp Stracker <p.stracker@syde.com>
2024-11-20 16:54:11 +0100

    ♻️ Simplify selectors

--- modules/ppcp-settings/resources/js/data/onboarding/selectors.js
+++ modules/ppcp-settings/resources/js/data/onboarding/selectors.js
@@ -3,32 +3,23 @@
  *
  * These functions provide a consistent interface for accessing store data.
  * They allow components to retrieve data without knowing the store structure.
- * Exported functions must have unique names across all store modules.
  *
  * @file
  */
 
-import { STORE_KEY } from './constants';
-
 const EMPTY_OBJ = Object.freeze( {} );
 
-const getState = ( state ) => {
-	if ( ! state ) {
-		return EMPTY_OBJ;
-	}
+const getState = ( state ) => state || EMPTY_OBJ;
 
-	return state[ STORE_KEY ] || EMPTY_OBJ;
-};
-
-export const onboardingPersistentData = ( state ) => {
+export const persistentData = ( state ) => {
 	return getState( state ).data || EMPTY_OBJ;
 };
 
-export const onboardingTransientData = ( state ) => {
+export const transientData = ( state ) => {
 	const { data, flags, ...transientState } = getState( state );
 	return transientState || EMPTY_OBJ;
 };
 
-export const onboardingFlags = ( state ) => {
+export const flags = ( state ) => {
 	return getState( state ).flags || EMPTY_OBJ;
 };
