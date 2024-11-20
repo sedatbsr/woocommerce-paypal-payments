e181892a3439a2478186bea110a1d8b71e544391
Philipp Stracker <p.stracker@syde.com>
2024-11-20 18:38:06 +0100

    ✨ Register the common store

--- modules/ppcp-settings/resources/js/data/index.js
+++ modules/ppcp-settings/resources/js/data/index.js
@@ -1,10 +1,15 @@
-import * as Onboarding from './onboarding';
 import { addDebugTools } from './debug';
+import * as Onboarding from './onboarding';
+import * as Common from './common';
 
 Onboarding.initStore();
+Common.initStore();
 
 export const OnboardingHooks = Onboarding.hooks;
+export const CommonHooks = Common.hooks;
+
 export const OnboardingStoreName = Onboarding.STORE_NAME;
+export const CommonStoreName = Common.STORE_NAME;
 
 export * from './constants';
 
