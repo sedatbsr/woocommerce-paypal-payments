1c27e9092356c489803af952542b884ec6f2c3aa
Philipp Stracker <p.stracker@syde.com>
2024-11-20 16:54:29 +0100

    ♻️ Simplify actions

--- modules/ppcp-settings/resources/js/data/onboarding/actions.js
+++ modules/ppcp-settings/resources/js/data/onboarding/actions.js
@@ -2,13 +2,15 @@
  * Action Creators: Define functions to create action objects.
  *
  * These functions update state or trigger side effects (e.g., async operations).
- * Exported functions must have unique names across all store modules.
  * Actions are categorized as Transient, Persistent, or Side effect.
  *
  * @file
  */
 
+import { select } from '@wordpress/data';
+
 import ACTION_TYPES from './action-types';
+import { STORE_NAME } from './constants';
 
 /**
  * @typedef {Object} Action An action object that is handled by a reducer or control.
@@ -21,9 +23,7 @@ import ACTION_TYPES from './action-types';
  *
  * @return {Action} The action.
  */
-export const resetOnboarding = () => {
-	return { type: ACTION_TYPES.RESET };
-};
+export const reset = () => ( { type: ACTION_TYPES.RESET } );
 
 /**
  * Transient. Marks the onboarding details as "ready", i.e., fully initialized.
@@ -31,12 +31,10 @@ export const resetOnboarding = () => {
  * @param {boolean} isReady
  * @return {Action} The action.
  */
-export const setIsReady = ( isReady ) => {
-	return {
-		type: ACTION_TYPES.SET_TRANSIENT,
-		payload: { isReady },
-	};
-};
+export const setIsReady = ( isReady ) => ( {
+	type: ACTION_TYPES.SET_TRANSIENT,
+	payload: { isReady },
+} );
 
 /**
  * Transient. Changes the "saving" flag.
@@ -44,12 +42,10 @@ export const setIsReady = ( isReady ) => {
  * @param {boolean} isSaving
  * @return {Action} The action.
  */
-export const setIsSaving = ( isSaving ) => {
-	return {
-		type: ACTION_TYPES.SET_TRANSIENT,
-		payload: { isSaving },
-	};
-};
+export const setIsSaving = ( isSaving ) => ( {
+	type: ACTION_TYPES.SET_TRANSIENT,
+	payload: { isSaving },
+} );
 
 /**
  * Transient. Changes the "manual connection is busy" flag.
@@ -57,12 +53,10 @@ export const setIsSaving = ( isSaving ) => {
  * @param {boolean} isBusy
  * @return {Action} The action.
  */
-export const setManualConnectionIsBusy = ( isBusy ) => {
-	return {
-		type: ACTION_TYPES.SET_TRANSIENT,
-		payload: { isBusy },
-	};
-};
+export const setManualConnectionIsBusy = ( isBusy ) => ( {
+	type: ACTION_TYPES.SET_TRANSIENT,
+	payload: { isBusy },
+} );
 
 /**
  * Persistent. Set the full onboarding details, usually during app initialization.
@@ -70,12 +64,10 @@ export const setManualConnectionIsBusy = ( isBusy ) => {
  * @param {{data: {}, flags?: {}}} payload
  * @return {Action} The action.
  */
-export const hydrateOnboardingDetails = ( payload ) => {
-	return {
-		type: ACTION_TYPES.HYDRATE,
-		payload,
-	};
-};
+export const hydrate = ( payload ) => ( {
+	type: ACTION_TYPES.HYDRATE,
+	payload,
+} );
 
 /**
  * Persistent.Set the "onboarding completed" flag which shows or hides the wizard.
@@ -83,12 +75,10 @@ export const hydrateOnboardingDetails = ( payload ) => {
  * @param {boolean} completed
  * @return {Action} The action.
  */
-export const setCompleted = ( completed ) => {
-	return {
-		type: ACTION_TYPES.SET_PERSISTENT,
-		payload: { completed },
-	};
-};
+export const setCompleted = ( completed ) => ( {
+	type: ACTION_TYPES.SET_PERSISTENT,
+	payload: { completed },
+} );
 
 /**
  * Persistent. Sets the onboarding wizard to a new step.
@@ -96,12 +86,10 @@ export const setCompleted = ( completed ) => {
  * @param {number} step
  * @return {Action} The action.
  */
-export const setOnboardingStep = ( step ) => {
-	return {
-		type: ACTION_TYPES.SET_PERSISTENT,
-		payload: { step },
-	};
-};
+export const setStep = ( step ) => ( {
+	type: ACTION_TYPES.SET_PERSISTENT,
+	payload: { step },
+} );
 
 /**
  * Persistent. Sets the sandbox mode on or off.
@@ -109,12 +97,10 @@ export const setOnboardingStep = ( step ) => {
  * @param {boolean} useSandbox
  * @return {Action} The action.
  */
-export const setSandboxMode = ( useSandbox ) => {
-	return {
-		type: ACTION_TYPES.SET_PERSISTENT,
-		payload: { useSandbox },
-	};
-};
+export const setSandboxMode = ( useSandbox ) => ( {
+	type: ACTION_TYPES.SET_PERSISTENT,
+	payload: { useSandbox },
+} );
 
 /**
  * Persistent. Toggles the "Manual Connection" mode on or off.
@@ -122,12 +108,10 @@ export const setSandboxMode = ( useSandbox ) => {
  * @param {boolean} useManualConnection
  * @return {Action} The action.
  */
-export const setManualConnectionMode = ( useManualConnection ) => {
-	return {
-		type: ACTION_TYPES.SET_PERSISTENT,
-		payload: { useManualConnection },
-	};
-};
+export const setManualConnectionMode = ( useManualConnection ) => ( {
+	type: ACTION_TYPES.SET_PERSISTENT,
+	payload: { useManualConnection },
+} );
 
 /**
  * Persistent. Changes the "client ID" value.
@@ -135,12 +119,10 @@ export const setManualConnectionMode = ( useManualConnection ) => {
  * @param {string} clientId
  * @return {Action} The action.
  */
-export const setClientId = ( clientId ) => {
-	return {
-		type: ACTION_TYPES.SET_PERSISTENT,
-		payload: { clientId },
-	};
-};
+export const setClientId = ( clientId ) => ( {
+	type: ACTION_TYPES.SET_PERSISTENT,
+	payload: { clientId },
+} );
 
 /**
  * Persistent. Changes the "client secret" value.
@@ -148,12 +130,10 @@ export const setClientId = ( clientId ) => {
  * @param {string} clientSecret
  * @return {Action} The action.
  */
-export const setClientSecret = ( clientSecret ) => {
-	return {
-		type: ACTION_TYPES.SET_PERSISTENT,
-		payload: { clientSecret },
-	};
-};
+export const setClientSecret = ( clientSecret ) => ( {
+	type: ACTION_TYPES.SET_PERSISTENT,
+	payload: { clientSecret },
+} );
 
 /**
  * Persistent. Sets the "isCasualSeller" value.
@@ -161,12 +141,10 @@ export const setClientSecret = ( clientSecret ) => {
  * @param {boolean} isCasualSeller
  * @return {Action} The action.
  */
-export const setIsCasualSeller = ( isCasualSeller ) => {
-	return {
-		type: ACTION_TYPES.SET_PERSISTENT,
-		payload: { isCasualSeller },
-	};
-};
+export const setIsCasualSeller = ( isCasualSeller ) => ( {
+	type: ACTION_TYPES.SET_PERSISTENT,
+	payload: { isCasualSeller },
+} );
 
 /**
  * Persistent. Sets the "products" array.
@@ -174,12 +152,10 @@ export const setIsCasualSeller = ( isCasualSeller ) => {
  * @param {string[]} products
  * @return {Action} The action.
  */
-export const setProducts = ( products ) => {
-	return {
-		type: ACTION_TYPES.SET_PERSISTENT,
-		payload: { products },
-	};
-};
+export const setProducts = ( products ) => ( {
+	type: ACTION_TYPES.SET_PERSISTENT,
+	payload: { products },
+} );
 
 /**
  * Side effect. Triggers the persistence of onboarding data to the server.
