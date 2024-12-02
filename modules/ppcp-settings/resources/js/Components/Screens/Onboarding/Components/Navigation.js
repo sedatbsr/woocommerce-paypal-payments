cecf92d1578307984207283fffbcb66b98cc20fa
Philipp Stracker <p.stracker@syde.com>
2024-12-02 17:46:07 +0100

    ♻️ Greatly improve Navigation component

--- modules/ppcp-settings/resources/js/Components/Screens/Onboarding/Components/Navigation.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Onboarding/Components/Navigation.js
@@ -4,107 +4,59 @@ import { __ } from '@wordpress/i18n';
 
 import { OnboardingHooks } from '../../../../data';
 
-const Navigation = ( {
-	setStep,
-	setCompleted,
-	currentStep,
-	stepperOrder,
-	title,
-} ) => {
-	const isLastStep = () => currentStep + 1 === stepperOrder.length;
-	const isFistStep = () => currentStep === 0;
-	const navigateBy = ( stepDirection ) => {
-		let newStep = currentStep + stepDirection;
+const Navigation = ( { stepDetails, onNext, onPrev, onExit } ) => {
+	const { title, isFirst, percentage, showNext, canProceed } = stepDetails;
 
-		if ( isNaN( newStep ) || newStep < 0 ) {
-			console.warn( 'Invalid next step:', newStep );
-			newStep = 0;
-		}
-
-		if ( newStep >= stepperOrder.length ) {
-			setCompleted( true );
-		} else {
-			setStep( newStep );
-		}
-	};
-
-	const { products } = OnboardingHooks.useProducts();
-	const { isCasualSeller } = OnboardingHooks.useBusiness();
-
-	let disabled = false;
-
-	switch ( currentStep ) {
-		case 1:
-			disabled = isCasualSeller === null;
-			break;
-		case 2:
-			disabled = products.length < 1;
-			break;
-	}
+	const state = OnboardingHooks.useNavigationState();
+	const isDisabled = ! canProceed( state );
 
 	return (
 		<div className="ppcp-r-navigation-container">
 			<div className="ppcp-r-navigation">
 				<div className="ppcp-r-navigation--left">
 					<Icon icon={ chevronLeft } />
-					{ ! isFistStep() ? (
-						<Button
-							variant="tertiary"
-							onClick={ () => navigateBy( -1 ) }
-						>
-							{ title }
-						</Button>
-					) : (
-						<a
-							className="ppcp-r-navigation--left__link"
-							href={ global.ppcpSettings.wcPaymentsTabUrl }
-							aria-label={ __(
-								'Return to payments',
-								'woocommerce-paypal-payments'
-							) }
-						>
-							{ title }
-						</a>
-					) }
+					<Button
+						variant="tertiary"
+						onClick={ isFirst ? onExit : onPrev }
+					>
+						{ title }
+					</Button>
 				</div>
-				{ ! isFistStep() && (
-					<div className="ppcp-r-navigation--right">
-						<a
-							href={ global.ppcpSettings.wcPaymentsTabUrl }
-							aria-label={ __(
-								'Return to payments',
-								'woocommerce-paypal-payments'
-							) }
-						>
-							{ __(
-								'Save and exit',
-								'woocommerce-paypal-payments'
-							) }
-						</a>
-						{ ! isLastStep() && (
-							<Button
-								variant="primary"
-								disabled={ disabled }
-								onClick={ () => navigateBy( 1 ) }
-							>
-								{ __(
-									'Continue',
-									'woocommerce-paypal-payments'
-								) }
-							</Button>
-						) }
-					</div>
-				) }
-				<div
-					className="ppcp-r-navigation--progress-bar"
-					style={ {
-						width: `${
-							( currentStep / ( stepperOrder.length - 1 ) ) * 90
-						}%`,
-					} }
-				></div>
+				{ ! isFirst &&
+					NextButton( { showNext, isDisabled, onNext, onExit } ) }
+				<ProgressBar percent={ percentage } />
 			</div>
 		</div>
+	);
+};
+
+const NextButton = ( { showNext, isDisabled, onNext, onExit } ) => {
+	return (
+		<div className="ppcp-r-navigation--right">
+			<Button onClick={ onExit }>
+				{ __( 'Save and exit', 'woocommerce-paypal-payments' ) }
+			</Button>
+			{ showNext && (
+				<Button
+					variant="primary"
+					disabled={ isDisabled }
+					onClick={ onNext }
+				>
+					{ __( 'Continue', 'woocommerce-paypal-payments' ) }
+				</Button>
+			) }
+		</div>
+	);
+};
+
+const ProgressBar = ( { percent } ) => {
+	percent = Math.min( Math.max( percent, 0 ), 100 );
+
+	return (
+		<div
+			className="ppcp-r-navigation--progress-bar"
+			style={ { width: `${ percent * 0.9 }%` } }
+		/>
 	);
 };
 
