aef5119ecd6a026c170bd916cedfabb20d23a1ce
Philipp Stracker <p.stracker@syde.com>
2024-12-02 17:44:11 +0100

    ✨ Consolidate navigation logic further

--- modules/ppcp-settings/resources/js/Components/Screens/Onboarding/availableSteps.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Onboarding/availableSteps.js
@@ -18,21 +18,25 @@ const ALL_STEPS = [
 		id: 'welcome',
 		title: __( 'PayPal Payments', 'woocommerce-paypal-payments' ),
 		StepComponent: StepWelcome,
+		canProceed: () => true,
 	},
 	{
 		id: 'business',
 		title: __( 'Set up store type', 'woocommerce-paypal-payments' ),
 		StepComponent: StepBusiness,
+		canProceed: ( { business } ) => business.isCasualSeller !== null,
 	},
 	{
 		id: 'products',
 		title: __( 'Select product types', 'woocommerce-paypal-payments' ),
 		StepComponent: StepProducts,
+		canProceed: ( { products } ) => products.products.length > 0,
 	},
 	{
 		id: 'methods',
 		title: __( 'Choose checkout options', 'woocommerce-paypal-payments' ),
 		StepComponent: StepPaymentMethods,
+		canProceed: () => true,
 	},
 	{
 		id: 'complete',
@@ -41,15 +45,26 @@ const ALL_STEPS = [
 			'woocommerce-paypal-payments'
 		),
 		StepComponent: StepCompleteSetup,
+		canProceed: () => true,
 	},
 ];
 
 export const getSteps = ( flags ) => {
-	if ( ! flags.canUseCasualSelling ) {
-		return ALL_STEPS.filter( ( step ) => 'business' !== step.id );
-	}
+	const steps = flags.canUseCasualSelling
+		? ALL_STEPS
+		: ALL_STEPS.filter( ( step ) => step.id !== 'business' );
 
-	return ALL_STEPS;
+	const totalStepsCount = steps.length;
+
+	return steps.map( ( step, index ) => ( {
+		...step,
+		isFirst: index === 0,
+		isLast: index === totalStepsCount - 1,
+		showNext: index < totalStepsCount - 1,
+		percentage: 100 * ( index / ( totalStepsCount - 1 ) ),
+		nextStep: index < totalStepsCount - 1 ? index + 1 : index,
+		prevStep: index > 0 ? index - 1 : 0,
+	} ) );
 };
 
 /**
